0af5778fb6068eb17342a8ef6e367049a57d3de5
Your Name <you@hotmail.com>
2022-05-23 14:58:01 +0500

    spec is also included by  both spec and stream are labeled as no attack  apps

--- test/mlp.py
+++ test/mlp.py
@@ -27,8 +27,9 @@ for file in file_list:
 
 	print(file);
 	mypd=pd.read_excel(file)
-	if(file == "2.xlsx"):
-		mypd=pd.concat([mypd]*9, ignore_index=True)
+	if(file == "2.xlsx") or ((file == "3.xlsx") ) :
+		print(file)
+		mypd=pd.concat([mypd]*5, ignore_index=True)
 
 	print(mypd.shape)
 	excl_list.append(mypd)
@@ -45,8 +46,8 @@ for excl_file in excl_list:
 
 df1=excl_merged.sample(frac=1)
 
-df1 = df1[['simulaton tick','L1 Instruction Cache Hits', 'L1 Instruction Cache Misses','L1 Data Cache Hits','L1 Data Cache Misses','Last Level Cache Hits','DRAM Page Hit Rate ','status']] 
-test_df1=df1[['L1 Instruction Cache Hits', 'L1 Instruction Cache Misses','L1 Data Cache Hits','L1 Data Cache Misses','Last Level Cache Hits','DRAM Page Hit Rate ','status']]
+df1 = df1[['simulaton tick','L1 Instruction Cache Hits', 'L1 Instruction Cache Misses','L1 Data Cache Hits','L1 Data Cache Misses','Last Level Cache Hits','Last Level Cache Misses','DRAM Page Hit Rate ','status']] 
+test_df1=df1[['L1 Instruction Cache Hits', 'L1 Instruction Cache Misses','L1 Data Cache Hits','L1 Data Cache Misses','Last Level Cache Hits','Last Level Cache Misses','DRAM Page Hit Rate ','status']]
 
 #print(df1)
 X, y = df1.values[:,1 :-1], df1.values[:, -1]
@@ -58,6 +59,7 @@ y = LabelEncoder().fit_transform(y)
 print(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
+print(y)
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 n_features = X_train.shape[1]
@@ -81,6 +83,8 @@ loss, acc = model.evaluate(X_test, y_test, verbose=0)
 print('Test Accuracy: %.3f' % acc)
 print('loss: %.3f' % loss)
 
+exit(0);
+
 
 total_count=0
 wrong_predict=0
@@ -92,8 +96,9 @@ with open('mycsv.csv', newline='') as f:
 for i in mylist:
 	status=i.pop()
 	m=list(np.float_(i))
-	print(m)
 	yhat= model.predict([m])
+	print(status)
+	print('Predicted: %.3f' % yhat)
 	total_count += 1
 	if( yhat < 0.3):
 		predict='RWH'
