b9bb884f8bc1234e1ada4c513a229eda4315c6df
Your Name <you@hotmail.com>
2022-05-30 15:37:18 +0500

    work in progress

--- MLT/CNN_T/vgg16.py
+++ MLT/CNN_T/vgg16.py
@@ -1,34 +1,41 @@
-# cannot easily visualize filters lower down
+
+# plot feature map of first conv layer for given image
+import tensorflow as tf 
 from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+
+from keras.models import Model
 from matplotlib import pyplot
+from numpy import expand_dims
 # load the model
 model = VGG16()
-# summarize filter shapes
-for layer in model.layers:
-	# check for convolutional layer
-	if 'conv' not in layer.name:
-		continue
-	# get filter weights
-	filters, biases = layer.get_weights()
-	print( filters.shape)
-'''
-# normalize filter values to 0-1 so we can visualize them
-f_min, f_max = filters.min(), filters.max()
-filters = (filters - f_min) / (f_max - f_min)
-# plot first few filters
-n_filters, ix = 6, 1
-for i in range(n_filters):
-	# get the filter
-	f = filters[:, :, :, i]
-	# plot each channel separately
-	for j in range(3):
+# redefine model to output right after the first hidden layer
+model = Model(inputs=model.inputs, outputs=model.layers[1].output)
+model.summary()
+# load the image with the required shape
+img = load_img('bird.jpg', target_size=(224, 224))
+# convert the image to an array
+img = img_to_array(img)
+# expand dimensions so that it represents a single 'sample'
+img = expand_dims(img, axis=0)
+# prepare the image (e.g. scale pixel values for the vgg)
+img = preprocess_input(img)
+# get feature map for first hidden layer
+feature_maps = model.predict(img)
+# plot all 64 maps in an 8x8 squares
+square = 8
+ix = 1
+for _ in range(square):
+	for _ in range(square):
 		# specify subplot and turn of axis
-		ax = pyplot.subplot(n_filters, 3, ix)
+		ax = pyplot.subplot(square, square, ix)
 		ax.set_xticks([])
 		ax.set_yticks([])
 		# plot filter channel in grayscale
-		pyplot.imshow(f[:, :, j], cmap='gray')
+		pyplot.imshow(feature_maps[0, :, :, ix-1], cmap='gray')
 		ix += 1
 # show the figure
 pyplot.show()
-'''
